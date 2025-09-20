db54a7943cea0694c90b194da8a641a08ea98c01
Nhyun Kim <nhyun5050@gmail.com>
2025-09-20 22:30:33 +0900

    8. Vue로 앱 재구축하기

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
 
-const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listEl = document.querySelector('ul');
+window.Vue.createApp({
+    data: function() {
+        return {
+            goals: [],
+            enteredValue: '',
+        };
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue);
+            this.enteredValue = '';
+        },
+    }
+}).mount('#app');
 
-function addGoal() {
-    const enteredValue = inputEl.value;
+//
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
+//
+// function addGoal() {
+//     const enteredValue = inputEl.value;
+//
+//     const listItemEl = document.createElement('li');
+//     listItemEl.textContent = enteredValue;
+//
+//     listEl.appendChild(listItemEl);
+//     inputEl.value = '';
+// }
+//
+// buttonEl.addEventListener('click', addGoal);
 
-    const listItemEl = document.createElement('li');
-    listItemEl.textContent = enteredValue;
 
-    listEl.appendChild(listItemEl);
-    inputEl.value = '';
-}
-
-buttonEl.addEventListener('click', addGoal);
